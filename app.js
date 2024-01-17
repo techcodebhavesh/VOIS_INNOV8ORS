03b1a882079a33c7725a888ec8d9ff688153145a
Bhavesh Agone <136712795+techcodebhavesh@users.noreply.github.com>
2024-01-18 01:54:47 +0530

    Update app.js

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ const express = require("express");
 const path = require("path");
 const app = express();
 const testRouter = require("./routers/test.router");
+const apikeyRouter = require("./routers/apikey.router");
 const cors = require("cors");
 
 const PORT = process.env.PORT || 5001;
@@ -13,5 +14,6 @@ app.use(cors());
 app.use(express.static(path.join(__dirname, "client/build")));
 
 app.use("/api/test", testRouter);
+app.use("/api/apikey", apikeyRouter);
 
 app.listen(PORT, () => console.log("Server running on " + PORT));

b2fcacb9498ce7c43819aff1ac0d7aafb215a9ac
Mayur Agarwal <mayur.agarwal22@vit.edu>
2024-02-10 21:33:17 +0530

    output dashboard card removed

--- client/src/components/OutputDashboard.jsx
+++ client/src/components/OutputDashboard.jsx
@@ -64,12 +64,12 @@ const OutputDashboard = ({ data }) => {
               {/* <WeeklyRevenue data={data} /> */}
           </div>
         </div>
-        <div className="suggestion-con">
+        {/* <div className="suggestion-con">
           <div className="card-dash col-4">
             <div className="card-header bg-warning text-center">
               <h1 className="h1dash">CARD TITLE</h1>
             </div>
-            {/* <svg
+            <svg
               data-name="Layer 1"
               xmlns="http://www.w3.org/2000/svg"
               viewBox="0 0 1200 120"
@@ -79,10 +79,10 @@ const OutputDashboard = ({ data }) => {
                 d="M0,0V46.29c47.79,22.2,103.59,32.17,158,28,70.36-5.37,136.33-33.31,206.8-37.5C438.64,32.43,512.34,53.67,583,72.05c69.27,18,138.3,24.88,209.4,13.08,36.15-6,69.85-17.84,104.45-29.34C989.49,25,1113-14.29,1200,52.47V0Z"
                 className="fill-success"
               ></path>
-            </svg> */}
+            </svg>
           
           </div>
-        </div>
+        </div> */}
       </div>
     </>
   );
